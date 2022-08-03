6f28e28b42e50513c10058cfb4636e99db353805
ecto0310 <ecto0310@gmail.com>
2022-08-04 01:57:37 +0900

    [add] IFFT

---
+++ src/ifft.c
@@ -0,0 +1,22 @@
+#include "../include/signal_processing/ifft.h"
+
+#include <complex.h>
+#undef I
+#include <math.h>
+#include <stdlib.h>
+
+#include "../include/signal_processing/fft.h"
+
+int ifft(double _Complex out[], int N, const double _Complex in[]) {
+    double _Complex *con_in = malloc(sizeof(double _Complex) * N);
+    double _Complex *con_out = malloc(sizeof(double _Complex) * N);
+    for (int i = 0; i < N; i++) {
+        con_in[i] = conj(in[i]);
+    }
+
+    fft(con_out, N, con_in);
+    for (int i = 0; i < N; i++) {
+        out[i] = conj(con_out[i]) / N;
+    }
+    return 0;
+}
